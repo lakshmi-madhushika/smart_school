d36ca80ad657f4dda708c57b10af71bebf367cfb
SLM samarapura <43149579+lakshmi-madhushika@users.noreply.github.com>
2020-07-31 08:53:28 +0530

    add box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,28 +5,58 @@
 
 
      <title>Profile</title>
+  <style>
+      .col-md-3 {
+        width: 100px;
+        height:100px;
+        margin: auto;
+        float:left ;
+        text-align:center;
+       margin-bottom: 10px;
 
+
+      }
+
+  </style>
 
 </head>
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="row">
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h1
+   </div>
+   <div class="col-md-3 bg-white shadow-lg" >
+      h2
+   </div>
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h3
+   </div>
 </div>
+
+<div class="row">
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h4
+   </div>
+   <div class="col-md-3 bg-white shadow-lg" >
+      h5
+   </div>
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h6
+   </div>
+</div>
+
+<div class="row">
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h7
+   </div>
+   <div class="col-md-3 bg-white shadow-lg" >
+      h8
+   </div>
+   <div class=" col-md-3 bg-white shadow-lg" >
+      h9
+   </div>
+</div>
+
 @endsection
-@extends('layouts.foot')
+
